afc815c64f6ec4a0c7021a0b71052e376d874451
Charlos16v <cdominguez@cifpfbmoll.eu>
2021-04-22 21:34:15 +0200

    feat: FilterManager attributes, Constructor, getFilterChain, setTask, setTaskAt and executeTasks methods

--- src/main/java/edu/pingpong/mollap/manager/FilterManager.java
+++ src/main/java/edu/pingpong/mollap/manager/FilterManager.java
@@ -1,4 +1,30 @@
 package edu.pingpong.mollap.manager;
 
+import edu.pingpong.mollap.filter.Filter;
+import edu.pingpong.mollap.target.Target;
+
 public class FilterManager {
+
+    private FilterChain filterChain = null;
+
+    public FilterManager(Target target) {
+        this.filterChain = new FilterChain();
+        this.filterChain.setTarget(target);
+    }
+
+    public FilterChain getFilterChain() {
+        return filterChain;
+    }
+
+    public void setTask(Filter task) {
+        getFilterChain().addTask(task);
+    }
+
+    public void setTaskAt(int position, Filter task) {
+        getFilterChain().addTaskAt(position, task);
+    }
+
+    public void executeTasks(String message) {
+        getFilterChain().execute(message);
+    }
 }
