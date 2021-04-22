fc87c1baea97850083b9167deb6b6b2113ee9dd8
Charlos16v <cdominguez@cifpfbmoll.eu>
2021-04-22 21:31:50 +0200

    feat: execute method from Autenticate class, that implements Filter.

--- src/main/java/edu/pingpong/mollap/filter/Autenticate.java
+++ src/main/java/edu/pingpong/mollap/filter/Autenticate.java
@@ -1,4 +1,13 @@
 package edu.pingpong.mollap.filter;
 
-public class Autenticate implements Filter{
+public class Autenticate implements Filter {
+
+
+    public Autenticate() {
+    }
+
+    @Override
+    public void execute(String message) {
+        System.out.println(message + " autenticated succesfully!");
+    }
 }
