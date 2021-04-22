a1515ca6daf17305a43eef9e465bc685fb1aaf67
Charlos16v <cdominguez@cifpfbmoll.eu>
2021-04-22 21:31:23 +0200

    feat: execute method from Vehicle class, that implements Target.

--- src/main/java/edu/pingpong/mollap/target/Vehicle.java
+++ src/main/java/edu/pingpong/mollap/target/Vehicle.java
@@ -1,4 +1,12 @@
 package edu.pingpong.mollap.target;
 
-public class Vehicle implements Target{
+public class Vehicle implements Target {
+
+    public Vehicle() {
+    }
+
+    @Override
+    public void execute(String message) {
+        System.out.println("Door opened for " + message + "!");
+    }
 }
