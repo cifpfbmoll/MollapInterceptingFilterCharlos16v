d58998148033030029adda18cddda3b1aa2a2dab
Charlos16v <cdominguez@cifpfbmoll.eu>
2021-04-22 21:31:56 +0200

    feat: execute method from Autorize class, that implements Filter.

--- src/main/java/edu/pingpong/mollap/filter/Autorize.java
+++ src/main/java/edu/pingpong/mollap/filter/Autorize.java
@@ -1,4 +1,12 @@
 package edu.pingpong.mollap.filter;
 
-public class Autorize implements Filter{
+public class Autorize implements Filter {
+
+    public Autorize() {
+    }
+
+    @Override
+    public void execute(String message) {
+        System.out.println(message + " autorized succesfully!");
+    }
 }
