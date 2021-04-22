d91a45ec98ff477771605dd3e2ac2ebc432c0abf
Charlos16v <cdominguez@cifpfbmoll.eu>
2021-04-22 21:34:51 +0200

    feat: Client Interface methods

--- src/main/java/edu/pingpong/mollap/client/Client.java
+++ src/main/java/edu/pingpong/mollap/client/Client.java
@@ -1,6 +1,10 @@
 package edu.pingpong.mollap.client;
 
+import edu.pingpong.mollap.manager.FilterManager;
+
 public interface Client {
 
+    void setFilterManager(FilterManager filterManager);
 
+    void sendRequest(String message);
 }
